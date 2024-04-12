7a1683ee3942c9e16a866ae2a7ccde0c1bd71e63
Andre Correa <andre.correa@oracle.com>
2024-04-12 12:28:33 -0300

    feat: automation config added

--- compartments/examples/vision/variables.tf
+++ compartments/examples/vision/variables.tf
@@ -14,77 +14,13 @@ variable "private_key_password" {default = ""}
 #-------------------------------------------------------------
 variable "compartments_configuration" {
   description = "The compartments configuration. Use the compartments attribute to define your topology. OCI supports compartment hierarchies up to six levels."
+  type = any
+}
+
+variable "automation_config" {
   type = object({
-    default_parent_id = string 
-    default_defined_tags = optional(map(string)) 
-    default_freeform_tags = optional(map(string)) 
-    enable_delete = optional(bool) 
-    compartments = map(object({
-      name          = string
-      description   = string
-      parent_id   = optional(string)
-      defined_tags  = optional(map(string))
-      freeform_tags = optional(map(string))
-      tag_defaults     = optional(map(object({
-        tag_id = string,
-        default_value = string,
-        is_user_required = optional(bool)
-      })))
-      children      = optional(map(object({
-        name          = string
-        description   = string
-        defined_tags  = optional(map(string))
-        freeform_tags = optional(map(string))
-        tag_defaults     = optional(map(object({
-            tag_id = string,
-            default_value = string,
-            is_user_required = optional(bool)
-          })))
-        children      = optional(map(object({
-          name          = string
-          description   = string
-          defined_tags  = optional(map(string))
-          freeform_tags = optional(map(string))
-          tag_defaults     = optional(map(object({
-            tag_id = string,
-            default_value = string,
-            is_user_required = optional(bool)
-          })))
-          children      = optional(map(object({
-            name          = string
-            description   = string
-            defined_tags  = optional(map(string))
-            freeform_tags = optional(map(string))
-            tag_defaults     = optional(map(object({
-              tag_id = string,
-              default_value = string,
-              is_user_required = optional(bool)
-            })))
-            children      = optional(map(object({
-              name          = string
-              description   = string
-              defined_tags  = optional(map(string))
-              freeform_tags = optional(map(string))
-              tag_defaults     = optional(map(object({
-                tag_id = string,
-                default_value = string,
-                is_user_required = optional(bool)
-              })))
-              children      = optional(map(object({
-                name          = string
-                description   = string
-                defined_tags  = optional(map(string))
-                freeform_tags = optional(map(string))
-                tag_defaults     = optional(map(object({
-                  tag_id = string,
-                  default_value = string,
-                  is_user_required = optional(bool)
-                })))
-              })))  
-            })))
-          })))
-        })))
-      })))  
-    }))
+    bucket_name = string
+    output_file_name = string 
   })
+  default = null
 }
