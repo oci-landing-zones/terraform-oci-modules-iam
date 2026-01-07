7d75741cda68ec23d5c55c49dfc03d2444d80c3e
Andre Correa <andre.correa@oracle.com>
2026-01-07 17:19:28 -0300

    Release 0.3.2 (#45)
    * Release 0.2.1
    
    * Release 0.2.2
    
    * feat: TF < 1.3.0 restriction removed
    
    * feat: TF binary equal or greater than 1.3.0 requirement added
    
    * chore: release notes and version bump
    
    * chore: release notes, tag and SPECs updated
    
    * fix typo in dynamic groups that refered to domain groups
    
    * fix: version = "<= 5.16.0" removed
    
    * chore: release notes, version and spec updated
    
    * feat: OCI FW and ZPR IAM policies added
    
    * feat: examples updated
    
    * chore: release notes and version update
    
    * chore: release notes updated.
    
    * fix: attribute_sets = ["all"] added to oci_identity_domains_group
    
    * chore: release notes and version increment
    
    * fix: preventing username dupes to fail user lookup
    
    * fix: ignoring username dupes that can be provided as input
    
    * chore: release notes a version increment
    
    * chore: release date updated
    
    * feat: debug flag added
    
    * fix: user lookup only checks ACTIVE users
    
    * chore: release notes updated
    
    * doc: SPEC.md updated
    
    * feat: members checked against their respective identity domains.
    
    * doc: spec updated
    
    * chore: release notes
    
    * fix: debug reporting removed
    
    * feat: support for ignoring or honoring external updates done to identity domain group memberships
    
    * chore: release notes
    
    * fix: precondition check for any members
    
    * fix: error_message fix
    
    * fix: error_message fix 2
    
    * fix: ignoring changes in schemas attribute
    
    * fix: ignore_changes list updated
    
    * fix: ignore_changes list added to dyn groups
    
    * feat: groups requestable by default
    
    * fix: schemas attribute alignment
    
    * fix: ignore_changes removed
    
    * fix: formatting
    
    * fix: removed attribute_sets=["all"]
    
    * feat: module default tag added
    
    * feat: policies updated: descriptions and tenancy level grants to app admins
    
    * chore: release notes updated
    
    * chore: release notes updated
    
    * chore: release notes updated
    
    * update to key value in order to create compartments_configuration and tags_configuration in one pass
    
    * release notes update
    
    * Update file RELEASE-NOTES.md
    
    * fix: user data source per identity domain and only evaluated if members attribute provided
    
    * fix: handling empty members list
    
    * chore: release notes updated
    
    * fix: looking up requested members only
    
    * chore: release data updated
    
    * Add pipeline code
    
    * Test 1
    
    * test 2
    
    * test 3
    
    * test 4
    
    * Testing CI config
    
    * test 4
    
    * Test .ci/.gitlab-ci.yml file
    
    * Rearranging files
    
    * Automated commit from pipeline [skip ci]
    
    * test 5
    
    * Automated commit from pipeline [skip ci]
    
    * test 6
    
    * Automated commit from pipeline [skip ci]
    
    * chore: release notes updated
    
    * Release 0.3.1
    
    * Lanz 4171
    
    * chore: release notes updated
    
    * Delete .ci directory
    
    Signed-off-by: Andre Correa <andre.correa@oracle.com>
    
    ---------
    
    Signed-off-by: Andre Correa <andre.correa@oracle.com>
    Co-authored-by: Rory Nguyen <rory.nguyen@oracle.com>
    Co-authored-by: josh_hammer <josh.hammer@oracle.com>
    Co-authored-by: Shelby Willis <shelby.willis@oracle.com>
    Co-authored-by: mianguye <mia.nguyen@oracle.com>
    Co-authored-by: terraform-runner <terraform-runner.oracle.com>
    Co-authored-by: Niya Ma <niya.m.ma@oracle.com>

--- identity-domains/variables.tf
+++ identity-domains/variables.tf
@@ -232,9 +232,9 @@ variable "identity_domain_applications_configuration" {
       fa_admin_password       = optional(string)
       fa_ssl_enabled          = optional(bool)
       fa_override_custom_sync = optional(bool)
-      fa_admin_roles          = optional(list(string)),
-      defined_tags  = optional(map(string)),
-      freeform_tags = optional(map(string))
+      fa_admin_roles          = optional(list(string))
+      defined_tags            = optional(map(string))
+      freeform_tags           = optional(map(string))
     }))
   })
   default = null
