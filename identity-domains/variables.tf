637e3995c6a24dd77ca83799344faf8a4d0e00dc
Peter Hrvola <peter.hrvola@oracle.com>
2026-09-21 20:01:38 +0200

    fix(identity-domains): disable requestable groups by default (#52)
    * fix(identity-domains): disable requestable groups by default
    
    * Update release notes for Identity Domains module
    
    Clarified the note regarding requestable identity domain groups and their default setting.
    
    Signed-off-by: Andre Correa <andre.correa@oracle.com>
    
    ---------
    
    Signed-off-by: Andre Correa <andre.correa@oracle.com>
    Co-authored-by: Andre Correa <andre.correa@oracle.com>

--- identity-domains/variables.tf
+++ identity-domains/variables.tf
@@ -45,7 +45,7 @@ variable "identity_domain_groups_configuration" {
       identity_domain_id = optional(string),
       name               = string,
       description        = optional(string),
-      requestable        = optional(bool, true),
+      requestable        = optional(bool, false),
       members            = optional(list(string), []),
       defined_tags       = optional(map(string)),
       freeform_tags      = optional(map(string))
